fd6985484b405453ec18d0d74aad560a5222e525
Manish Kumar <k.manu00005@gmail.com>
2023-11-14 23:49:44 +0530

    14-Nov

---
+++ src/test/OOD/DesignPattern/SingeltonPattern/EagerInitialization/CacheService.java
@@ -0,0 +1,25 @@
+package test.OOD.DesignPattern.SingeltonPattern.EagerInitialization;
+
+import java.util.HashMap;
+
+// Singleton Class for Cache Service - Eager Implementation.
+public class CacheService {
+    private static CacheService service = new CacheService();
+    private HashMap<Integer, String> cacheMap;
+
+    private CacheService() {
+        cacheMap = new HashMap<Integer, String>();
+    }
+
+    public static CacheService getCache() {
+        return service;
+    }
+
+    public String getValue(int key) {
+        return cacheMap.getOrDefault(key, "");
+    }
+
+    public void store(int key, String value) {
+        cacheMap.put(key, value);
+    }
+}
