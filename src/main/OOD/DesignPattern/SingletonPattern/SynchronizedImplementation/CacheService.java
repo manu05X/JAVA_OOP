44b83dbfb135baf17bf35f8d75fe3a849febfba2
Manish Kumar <k.manu00005@gmail.com>
2023-11-15 18:25:15 +0530

    Singleton Pattern

---
+++ src/main/OOD/DesignPattern/SingletonPattern/SynchronizedImplementation/CacheService.java
@@ -0,0 +1,27 @@
+package main.OOD.DesignPattern.SingletonPattern.SynchronizedImplementation;
+import java.util.HashMap;
+
+// Singleton Class for Cache Service - Synchronized Implementation.
+public class CacheService {
+    private static CacheService service = null;
+    private HashMap<Integer, String> cacheMap;
+
+    private CacheService() {
+        cacheMap = new HashMap<Integer, String>();
+    }
+
+    public static synchronized CacheService getCache() {
+        if (service == null) {
+            service = new CacheService();
+        }
+        return service;
+    }
+
+    public String getValue(int key) {
+        return cacheMap.getOrDefault(key, "");
+    }
+
+    public void store(int key, String value) {
+        cacheMap.put(key, value);
+    }
+}
---
+++ src/main/OOD/DesignPattern/SingletonPattern/SynchronizedImplementation/Runner.java
@@ -0,0 +1,19 @@
+package main.OOD.DesignPattern.SingletonPattern.SynchronizedImplementation;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("============== Synchronized Implementation ============== \n");
+        CacheService service = CacheService.getCache();
+        System.out.println("Got the instance for the first time \n");
+
+        service.store(1, "Saurav");
+        System.out.println("Added a key value pair: [1, Saurav] \n");
+
+        CacheService secondService = CacheService.getCache();
+        System.out.println("Got the instance for the second time \n");
+
+        String value = secondService.getValue(1);
+        System.out.println("Value returned from the second Cache service: " + value);
+        System.out.println("This proves that a single instance will be returned everytime. \n");
+    }
+}
